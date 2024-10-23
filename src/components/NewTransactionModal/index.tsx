0559a24ecd973943dbca2e91f188d6c50ee08d6c
wellmota <impeto.wellington@gmail.com>
2024-10-23 20:15:52 -0300

    Refactor NewTransactionModal component to include transaction type buttons

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,12 @@
 import * as Dialog from "@radix-ui/react-dialog"
-import { CloseButton, Content, Overlay } from "./styles"
-import { X } from "phosphor-react"
+import {
+  CloseButton,
+  Content,
+  Overlay,
+  TransactionType,
+  TransactionTypeButton,
+} from "./styles"
+import { ArrowCircleDown, ArrowCircleUp, X } from "phosphor-react"
 
 export function NewTransactionModal() {
   return (
@@ -17,6 +23,18 @@ export function NewTransactionModal() {
           <input type="text" placeholder="Decription" required />
           <input type="number" placeholder="Price" required />
           <input type="text" placeholder="Category" required />
+
+          <TransactionType>
+            <TransactionTypeButton variant="income">
+              <ArrowCircleUp />
+              Income
+            </TransactionTypeButton>
+            <TransactionTypeButton variant="outcome">
+              <ArrowCircleDown />
+              Outcome
+            </TransactionTypeButton>
+          </TransactionType>
+
           <button type="submit">Create</button>
         </form>
       </Content>
