a7a928eecc53c84255b98a4476dc80fd4c1b3fb8
wellmota <impeto.wellington@gmail.com>
2024-10-23 17:25:55 -0300

    Refactor package.json to add @radix-ui/react-dialog dependency

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,24 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles"
 import logoImg from "../../assets/logo.svg"
+import * as Dialog from "@radix-ui/react-dialog"
 
 export function Header() {
   return (
     <HeaderContainer>
       <HeaderContent>
         <img src={logoImg} alt="" />
-        <NewTransactionButton>New Transaction</NewTransactionButton>
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionButton>New Transaction</NewTransactionButton>
+          </Dialog.Trigger>
+          <Dialog.Portal>
+            <Dialog.Overlay/>
+            <Dialog.Content>
+              <Dialog.Title>New Transaction</Dialog.Title>
+              <Dialog.Close/>
+            </Dialog.Content>
+          </Dialog.Portal>
+        </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
   )
