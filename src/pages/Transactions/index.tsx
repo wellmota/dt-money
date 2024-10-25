a771d5d68a1b9822c646a42d4c605e8b6cf151c1
wellmota <impeto.wellington@gmail.com>
2024-10-25 14:41:38 -0300

    Refactor App component to use TransactionsProvider context

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,4 +1,4 @@
-import { useInsertionEffect, useState } from "react"
+import { useContext, useEffect, useState } from "react"
 import { Header } from "../../components/Header"
 import { Summary } from "../../components/Summary"
 import { SearchForm } from "./components/SearchForm"
@@ -7,6 +7,7 @@ import {
   TransactionsContainer,
   TransactionsTable,
 } from "./styles"
+import { TransactionsContext } from "../../contexts/TransactionsContext"
 
 interface Transaction {
   id: number
@@ -18,17 +19,7 @@ interface Transaction {
 }
 
 export function Transactions() {
-  const [transactions, setTransactions] = useState<Transaction[]>([])
-
-  async function loadTransactions() {
-    const response = await fetch("http://localhost:3333/transactions")
-    const data = await response.json()
-    setTransactions(data)
-  }
-
-  useInsertionEffect(() => {
-    loadTransactions()
-  }, [])
+  const { transactions } = useContext(TransactionsContext)
 
   return (
     <div>
