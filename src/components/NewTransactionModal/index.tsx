403edab504b4d43b50c1cae2d60ab053c59c34de
wellmota <impeto.wellington@gmail.com>
2024-11-09 22:54:41 -0300

    Add new transactions to server.json and update NewTransactionModal to create transactions via API

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -10,6 +10,7 @@ import { ArrowCircleDown, ArrowCircleUp, X } from "phosphor-react"
 import * as z from "zod"
 import { Controller, useForm } from "react-hook-form"
 import { zodResolver } from "@hookform/resolvers/zod"
+import { api } from "../../lib/axios"
 
 const newTransactionFormSchema = z.object({
   description: z.string(),
@@ -26,6 +27,7 @@ export function NewTransactionModal() {
     register,
     handleSubmit,
     formState: { isSubmitting },
+    reset,
   } = useForm<NewTransactionFormInputs>({
     resolver: zodResolver(newTransactionFormSchema),
     defaultValues: {
@@ -34,8 +36,17 @@ export function NewTransactionModal() {
   })
 
   async function handleCreateNewTransaction(data: NewTransactionFormInputs) {
-    await new Promise((resolve) => setTimeout(resolve, 2000))
-    console.log(data)
+    const { category, description, price, type } = data
+
+    await api.post("transactions", {
+      category,
+      description,
+      price,
+      type,
+      createdAt: new Date(),
+    })
+
+    reset()
   }
 
   return (
